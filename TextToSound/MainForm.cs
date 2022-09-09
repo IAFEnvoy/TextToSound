703c5db25dca8d2e6ed3883ac44f11c0988b77ce
Administrator <Administrator@DESKTOP-LHTMB71>
2022-09-09 16:08:08 +0800

    添加项目文件。

---
+++ TextToSound/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TextToSound {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            using(SpeechSynthesizer synth = new SpeechSynthesizer()) {
+                synth.Speak(textBox1.Text);  
+            }
+        }
+    }
+}
